b2ad20962320c6097885b5ba77254ee1978e18fe
smjoy222 <joy15-5777@diu.edu.bd>
2025-02-18 10:18:00 +0600

    task 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,43 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const Labclass05());
+  runApp(const Lab5());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Lab5 extends StatelessWidget {
+  const Lab5({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp
+    return MaterialApp(
+      title: "Basic UI",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("basic UI"),
+          backgroundColor: Colors.blue[400],
+          centerTitle: true,
+        ),
+        body: Row(
+          children: [
+            Text(
+              'HI, ',
+              style: TextStyle(
+                color: Colors.red,
+                fontSize: 30,
+              ),),
+            Text(
+              'This is',
+              style: TextStyle(
+                color: Colors.yellow,
+                fontSize: 50,
+              ),),
+            Text(
+              'flutter',
+              style: TextStyle(
+                color: Colors.blue,
+                fontSize: 20,
+              ),)],
+        ),
+      )
+    )
