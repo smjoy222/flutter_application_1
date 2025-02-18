704b53e95a28b473740e9d3cb39fa4433a3713a6
smjoy222 <joy15-5777@diu.edu.bd>
2025-02-18 10:20:02 +0600

    task 1

--- lib/main.dart
+++ lib/main.dart
@@ -11,33 +11,39 @@ class Lab5 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Basic UI",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("basic UI"),
-          backgroundColor: Colors.blue[400],
-          centerTitle: true,
-        ),
-        body: Row(
-          children: [
-            Text(
-              'HI, ',
-              style: TextStyle(
-                color: Colors.red,
-                fontSize: 30,
-              ),),
-            Text(
-              'This is',
-              style: TextStyle(
-                color: Colors.yellow,
-                fontSize: 50,
-              ),),
-            Text(
-              'flutter',
-              style: TextStyle(
-                color: Colors.blue,
-                fontSize: 20,
-              ),)],
-        ),
-      )
-    )
+        title: "Basic UI",
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("basic UI"),
+            backgroundColor: Colors.blue[400],
+            centerTitle: true,
+          ),
+          body: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.star,
+                size: 50,
+              ),
+               Icon(
+                Icons.star,
+                size: 50,
+              ),
+               Icon(
+                Icons.star,
+                size: 50,
+              ),
+               Icon(
+                Icons.star,
+                size: 50,
+              ),
+               Icon(
+                Icons.star_outline,
+                size: 50,
+              ),
+              
+            ],
+          ),
+        ));
+  }
+}
